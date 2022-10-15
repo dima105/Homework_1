79d3f519716704ee1fd22e2452a9898673751fc8
Dmitry_Kulik <dima27121985@gmail.com>
2022-10-15 17:21:39 +0300

    aaaaa

---
+++ index.js
@@ -0,0 +1,77 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+// function should return two numbers
+const sum = (a, b) => {
+  return a + b;
+};
+
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+// function should return if number is even
+const isNumberEven = (num) => {
+  // write code here
+};
+
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {string}
+ */
+// Write logic to compare two numbers and return which number is greater or they are equal
+const findLargestNumber = (num1, num2) => {
+  // write code here
+};
+
+/**
+ * @param {number} side1
+ * @param {number} side2
+ * @param {number} side3
+ * @return {string}
+ */
+// function should return type of triangle
+const findTriangleType = (side1, side2, side3) => {
+  // write code here
+};
+
+/**
+ * @param {number} month
+ * @param {number} year
+ * @return {string}
+ */
+//function should return amount of days in month
+const findDaysInMonth = (month, year) => {
+  // write code here
+}
+
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @param {string} operation
+ * @return {number | string}
+ */
+const calculateResult = (num1, num2, operation) => {
+  // write code here
+}
+
+/**
+ * @return {string}
+ */
+// Create multiply table multiplying all digits from 2 t0 9 on 1 - 10 and write result in string
+const getMultiplicationTable = () => {
+  // write code here
+};
+
+module.exports = {
+  isNumberEven,
+  findLargestNumber,
+  findTriangleType,
+  findDaysInMonth,
+  sum,
+  calculateResult,
+  getMultiplicationTable,
+};
